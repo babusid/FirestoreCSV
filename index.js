bcc3fb6284e6a2f8ebf4360ce123d01cdbf3f036
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-03-15 21:18:06 -0500

    added CSV parsing code to upload routine, added prompting for multi-collection or single-collection sheets

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const admin = require('firebase-admin');
 const { firestore } = require('firebase-admin');
 const readline = require("readline");
-const questionAsync = require("./readline-ext");
+require("./readline-ext"); //async extension for readline
 const fs = require('fs');
+const csvParser = require('csv-parser');
 const databaseData = require('./info.json');
 
 
 
-const init = async()=>{
+const main = async()=>{
    console.log("For ease of use, I recommend having the following two files in the same directory as this program: \ninfo.json, and the CSV file you are using");
     //create input interface
    const input = readline.createInterface(
         process.stdin,
         process.stdout
     );
-    //find csv filepath and datamode to use
+    //find csv filepath, datamode, filemode to use
     const dataCSV = await input.questionAsync("What is the Filepath of the CSV file?: ");
-    let dataMode = await input.questionAsync("What is the datamode (Upload/Download)?: ");
+    let dataMode = await input.questionAsync("What is the data mode (Upload/Download)?: ");
     while(1){
         if(dataMode!="Upload"&&dataMode!="Download"){
             console.log("You must pick either 'Upload' or 'Download' (it is case-sensitive)");
@@ -25,63 +26,91 @@ const init = async()=>{
             break;
         }
     }
+    let fileMode = await input.questionAsync("What is the file mode? Does all of the data in this CSV go into/come from the same collection, or is it a multicollection sheet? (1/2)");
+    while(1){
+        if(fileMode!="1"&&fileMode!="2"){
+            console.log("You must pick either 1 or 2 to indicate the file mode");
+            fileMode = await input.questionAsync("What is the file mode? Does all of the data in this CSV go into the same collection, or is it a multicollection sheet? (1/2)");
+        } else {
+            break;
+        }
+    }
+    if (fileMode=="1"){
+        collection=await input.questionAsync("What is the collection name?")
+    }else{
+        console.log("Since you have selected multi-collection, this program will be slower.")
+    }
     
     //setup database credentials
-    const serviceAccount = databaseData.serviceAccount;
-    const databaseURL = databaseData.databaseURL;
-    try{
-        admin.initializeApp({
-            credential: serviceAccount,
-            databaseURL: databaseURL,
-        });
-    } catch (error){
-        console.log("Did you fill out info.json?");
-        console.log("ERROR FROM FIRESTORE SDK:");
-        console.log(error);
-        process.exit(1);
-    }
-    const db = admin.firestore;
+    // const serviceAccount = databaseData.serviceAccount;
+    // const databaseURL = databaseData.databaseURL;
+    // try{
+    //     admin.initializeApp({
+    //         credential: serviceAccount,
+    //         databaseURL: databaseURL,
+    //     });
+    // } catch (error){
+    //     console.log("Did you fill out info.json?");
+    //     console.log("ERROR FROM FIRESTORE SDK:");
+    //     console.log(error);
+    //     process.exit(1);
+    // }
+    // const db = admin.firestore;
 
-    //run main func with filepath
-    main(dataCSV,dataMode,db);
-}
-
-/**
- * 
- * @param {String} dataCSV Filepath to Data Location
- * @param {String} dataMode Whether or not data should be Uploaded from firestore or uploaded to firestore
- * @param {admin.firestore} db Firestore Database to upload/download from
- */
-const main = (dataCSV,dataMode,db) =>{
     let filestream;
     if (dataMode=="Upload"){
-        try{
-            filestream = fs.createReadStream(dataCSV, { flags: "r" });
-        } catch(err){
-            console.log("The Given CSV file does not seem to exist. \nPlease ensure you have provided a valid CSV file, and relaunch this application. \nThis window will quit in 5 seconds")
-            setTimeout(()=>{
-                process.exit(0)
-            },5000)
-        }
+        filestream = fs.createReadStream(dataCSV, { flags: "r" }).on('error',()=>{
+            console.log("You seem to have input a non-existent filepath.\n Please verify filepath is correct and run this program again.\n This process will exit in 10 seconds")
+            setTimeout(() => {
+                process.exit(0);
+            }, 10000);
+        });
         csvToFirestore(filestream,db);
     } else {
         console.log("Download Mode is not yet built.")
         process.exit(0);
         firestoreToCSV(filestream,db);
     }
-
 }
+
 
 /**
  * @brief Function to upload CSV file to the firestore database
- * @param {fs.Interface.createReadStream} filestream  Filesystem readstream to use in order to parse CSV and upload to firestore database
+ * @param {fs.ReadStream} filestream  Filesystem readstream to use in order to parse CSV and upload to firestore database
  * @param {admin.firestore} db Firestore Database
  */
-const csvToFirestore =(filestream,db)=>{
+const csvToFirestore = async(filestream,db)=>{
+    let docArr = [];
+    //make sure that filestream is open
+    await new Promise(
+        (resolve)=>{
+            filestream.on('open', resolve)
+        }
+    );
+    //create filestream pipe to csvParser
+    const csvParse = filestream.pipe(csvParser());
+    //parse each row of csv into json and push into an array
+    csvParse.on
+    ('data',
+        (row)=>{
+            docArr.push(row);
+        }
+    );
+    //now that docArray is filled, go through, establish a reference to the collection of each entry, and upload the doc to that collection
+    if (fileMode=="1"){
+        //single collection sheet
+        csvParse.on('end',async()=>{
+            await db.CollectionReference()
+            console.log(docArr);
+            process.exit(0);
+        });
+    } else if (fileMode=="2"){
+        //multicollection sheet
+    }
     
 }
 
 
 
 //Runtime
-init();
+main();
