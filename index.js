f6776d018b721dcce7b2de96dc667e17f9e3f7f0
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-03-16 20:50:14 -0500

    Fixed Issue#1 (#6)
    * broke out CSVtoFirestore into a seperate module,
    introduced split between uploading single collection and multi-collection

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const { firestore } = require('firebase-admin');
 const readline = require("readline");
 require("./readline-ext"); //async extension for readline
 const fs = require('fs');
-const csvParser = require('csv-parser');
 const databaseData = require('./info.json');
-
+const csvToFirestore = require('./__components/CSVtoFirestore.js');
 
 const main = async()=>{
    console.log("For ease of use, I recommend having the CSV file you are using in the same directory as this package.");
@@ -26,7 +25,7 @@ const main = async()=>{
             break;
         }
     }
-    let fileMode = await input.questionAsync("What is the file mode? Does all of the data in this CSV go into/come from the same collection, or is it a multicollection sheet? (1/2)");
+    let fileMode = await input.questionAsync("What is the file mode? (Does all of the data in this CSV go into/come from the same collection, or is it a multicollection sheet?) (1/2)");
     while(1){
         if(fileMode!="1"&&fileMode!="2"){
             console.log("You must pick either 1 or 2 to indicate the file mode");
@@ -39,7 +38,10 @@ const main = async()=>{
     if (fileMode=="1"){
         collection=await input.questionAsync("What is the collection name?");
     }else{
-        console.log("Since you have selected multi-collection, this program will be slower.");
+        console.log("Multi-collection support has not yet been built. This program will stop in ten seconds.");
+        setTimeout(() => {
+            process.exit(1)
+        }, 10000);
     }
     
     //setup database credentials and initialize
@@ -54,76 +56,14 @@ const main = async()=>{
         process.exit(1);
     }
     const db = admin.firestore();
-
-    let filestream;
     if (dataMode=="Upload"){
-        filestream = fs.createReadStream(dataCSV, { flags: "r" }).on('error',()=>{
-            console.log("You seem to have input a non-existent filepath.\n Please verify filepath is correct and run this program again.\n This process will exit in 10 seconds");
-            setTimeout(() => {
-                process.exit(0);
-            }, 10000);
-        });
-        csvToFirestore(filestream,fileMode,collection,db);
+        if (fileMode=="1"){csvToFirestore.uploadSingleCollection(collection,db);}
+        
     } else {
         console.log("Download Mode is not yet built.");
-        process.exit(0);
+        process.exit(1);
         firestoreToCSV(filestream,db);
     }
-}
-
-
-/**
- * @brief Function to upload CSV file to the firestore database
- * @param {fs.ReadStream} filestream  Filesystem readstream to use in order to parse CSV and upload to firestore database
- * @param {String} filemode String 1/2 which chooses between single-collection CSV and multi-collection CSV where each row has the collection name 
- * @param {String} collection String which holds the collection name if single-collection, else is undefined
- * @param {firestore.Firestore} db Firestore Database
- */
-const csvToFirestore = async(filestream,fileMode,collection,db)=>{
-    let docArr = []; //holds all the rows of the CSV as individual document objects
-    //make sure that filestream is open
-    await new Promise(
-        (resolve)=>{
-            filestream.on('open', resolve);
-        }
-    );
-    //create filestream pipe to csvParser
-    const csvParse = filestream.pipe(csvParser());
-    //parse each row of csv into json and push into an array
-    csvParse.on
-    ('data',
-        (row)=>{
-            docArr.push(row);
-            //console.log(docArr);
-        }
-    );
-    //now that docArray is filled, go through, establish a reference to the collection of each entry, and upload the doc to that collection
-    csvParse.on('end',
-    async()=>{
-        console.log("Finished Parsing CSV");
-        if (fileMode=="1"){ //single-collection sheet
-            let promiseArr = []
-            docArr.map(
-                async(elem)=>{
-                    //TODO: add overwrite protection
-                    const elemProm = db.collection(collection).doc(`${elem.id}`).set(elem, {merge: true});
-                    promiseArr.push(elemProm);
-                    
-                })
-            //wait for promiseArr to resolve everything here and then exit
-            Promise.all(promiseArr).then(
-                ()=>{
-                    process.exit(0)
-                }  
-            );
-
-        } else if (fileMode=="2"){
-            //multicollection sheet
-            console.log("MultiCollection sheets are not yet supported");
-            process.exit(0);
-        }
-    }
-    )
 }
 
 //Runtime
